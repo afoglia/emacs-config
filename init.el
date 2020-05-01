a85a4ed6fa17300d38ed6f6e846d8a441a2b11a3
Anthony Foglia <afoglia@gmail.com>
2020-05-01 13:21:59 -0400

    Configure objc-mode

--- init.el
+++ init.el
@@ -834,6 +834,35 @@ Example:
             )
           )
 
+;;; Objective-C mode
+;;;
+;;; Configuration taken from <https://www.emacswiki.org/emacs/ObjectiveCMode>
+(add-to-list 'auto-mode-alist '("\\.m\\'" . objc-mode))
+(add-to-list 'auto-mode-alist '("\\.mm\\'" . objc-mode))
+;; Use objc-mode for objective-c headers
+(add-to-list 'magic-mode-alist
+             `(,(lambda ()
+                  (and (string= (file-name-extension buffer-file-name) "h")
+                       (re-search-forward "@\\<interface\\>"
+                                          magic-mode-regexp-match-limit t)))
+               . objc-mode))
+;; Configure cc-other-file-alist to know .h can be headers for .m and .mm files.
+(require 'find-file)
+(nconc (cadr (assoc "\\.h\\'" cc-other-file-alist)) '(".m" ".mm"))
+(defadvice ff-get-file-name (around ff-get-file-name-framework
+                                    (search-dirs
+                                     fname-stub
+                                     &optionak suffix-list))
+  "Search for Mac framework headers as well as POSIX headers."
+  (or
+   (if (string-match "\\(.*?\\)/\\(.*\\)" fname-stub)
+       (let* ((framework (match-string 1 fname-stub))
+              (header (match-string 2 fname-stub))
+              (fname-stub (concat framework ".framework/Headers/" header)))
+         ad-do-it))
+   ad-do-it))
+(ad-enable-advice 'ff-get-file-name 'around 'ff-get-file-name-framework)
+(ad-activate 'ff-get-file-name)
 
 ; Jamfile mode
 (require 'jam-mode nil 'noerror)
