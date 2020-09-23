770cb5c2e7d671954d8d33b33424960723a63977
Anthony Foglia <afoglia@gmail.com>
2020-09-23 09:49:33 -0400

    Set up display-fill-column-indicator-mode in markdown mode

--- init.el
+++ init.el
@@ -851,7 +851,12 @@ Example:
 ;;
 ;;; TODO: Move up in the order
 (if (>= emacs-major-version 27)
-    (add-hook 'prog-mode-hook 'display-fill-column-indicator-mode)
+    (progn
+      (add-hook 'prog-mode-hook '(lambda () (display-fill-column-indicator-mode)))
+      (add-hook 'prog-mode-hook '(lambda () (message "Running prog-mode-hook: buffer %s" (buffer-name))))
+      (when (try-require 'markdown-mode)
+        (add-hook 'markdown-mode-hook '(lambda () (message "Running markdown mode hooks: buffer %s" (buffer-name))))
+        (add-hook 'markdown-mode-hook #'display-fill-column-indicator-mode)))
   (when (try-require 'fill-column-indicator)
     (setq fci-rule-color "firebrick3")
 
