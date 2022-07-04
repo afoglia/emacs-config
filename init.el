6551b68c3204615f2ccdcae30c92bd51070be568
Anthony Foglia <afoglia@gmail.com>
2022-07-04 15:26:30 -0400

    Configure which-func-mode to use header line rather than mode line

--- init.el
+++ init.el
@@ -1303,7 +1303,20 @@ wide enough to show the indicator"
 ;; list.
 (if (listp which-func-modes)
     (add-to-list 'which-func-modes 'python-mode)
-)
+  )
+;; From https://emacsredux.com/blog/2014/04/05/which-function-mode/
+;; and https://www.emacswiki.org/emacs/WhichFuncMode
+(setq mode-line-misc-info (delete (assoc 'which-func-mode
+                                         mode-line-misc-info)
+				  mode-line-misc-info)
+      which-func-header-line-format '(which-func-mode ("" which-func-format)))
+(defadvice which-func-ff-hook (after header-line activate)
+  (when which-func-mode
+    (setq mode-line-misc-info (delete (assoc 'which-func-mode
+                                             mode-line-misc-info)
+				      mode-line-misc-info)
+          header-line-format which-func-header-line-format)))
+
 
 ; python mode customization
 (add-hook 'python-mode-hook
